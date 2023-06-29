708f54c7e45e27df09fef099431c46d4fe4b0bd8
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-06-29 19:53:54 +0300

    Add time before moving

--- Game/Assets/_Source/ParasiteSystem/Parasite.cs
+++ Game/Assets/_Source/ParasiteSystem/Parasite.cs
@@ -1,23 +1,45 @@
+using System.Collections;
 using UnityEngine;
 
 namespace ParasiteSystem
 {
     public class Parasite : MonoBehaviour
     {
+        [SerializeField] private int restoringMovementTime;
         [SerializeField] private LayerMask human;
+
+        private bool _canTransition = true;
+
+        private const float WAIT = 1f;
 
         void Update()
         {
-            RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, 4f, Vector2.up, 0, human);
-            // Debug.Log(hit[0].transform.name);
+            RaycastHit2D[] hit = Physics2D.CircleCastAll(transform.position, 2f, Vector2.up, 0, human);
+            
             if (hit.Length > 0
-                && Input.GetMouseButtonDown(0))
+                && Input.GetMouseButtonDown(0)
+                && _canTransition)
             {
-                // Debug.Log(hit[0].transform.name);
                 transform.parent.gameObject.SetActive(false);
                 transform.parent = hit[0].transform;
+                transform.parent.gameObject.layer = 7;
                 transform.localPosition = Vector3.zero;
+                StartCoroutine(RestoringMovement());
             }
+        }
+
+        private IEnumerator RestoringMovement()
+        {
+            _canTransition = false;
+            
+            int time = restoringMovementTime;
+            while (time != 0)
+            {
+                yield return new WaitForSeconds(WAIT);
+                time--;
+            }
+
+            _canTransition = true;
         }
     }
 }
